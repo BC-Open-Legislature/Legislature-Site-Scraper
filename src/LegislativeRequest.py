3df8a574277485f23461a3d2acdbf6b8db32e346
Erox1 <InfinityWaffles@outlook.com>
2021-10-18 23:44:41 -0700

    Initial Working Commit
    The initial commit with the first working version.

---
+++ src/LegislativeRequest.py
@@ -0,0 +1,12 @@
+import json
+import time
+
+# -=- BC Data Collector -=-
+from Provinces.BC.MainRequest import MainRequest as BC
+
+# -=- Get Credentials -=-
+with open('./secrets.json', 'r') as secrets_file:
+    secrets = json.load(secrets_file)['MongoCreds']
+
+# -=- Fetch Data -=-
+BC.get_daily_data(secrets)
